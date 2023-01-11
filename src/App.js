dde5ef5d06e79600c1bf8a025886b311ffa7e42a
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-11 13:29:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NewTicket from "./pages/NewTicket";
+import PrivateRoute from "./components/PrivateRoute";
+import Tickets from "./pages/Tickets";
+import Ticket from "./pages/Ticket";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
+            <Route path="/tickets" element={<PrivateRoute />}>
+              <Route path="/tickets" element={<Tickets />} />
+            </Route>
+            <Route path="/ticket/:ticketId" element={<PrivateRoute />}>
+              <Route path="/ticket/:ticketId" element={<Ticket />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Ticket.jsx
@@ -0,0 +1,155 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { closeTicket, getTicket } from "../features/tickets/ticketSlice";
+import {
+  createNote,
+  getNotes,
+  reset as notesReset,
+} from "../features/notes/noteSlice";
+import NoteItem from "../components/NoteItem";
+import Modal from "react-modal";
+import { FaPlus } from "react-icons/fa";
+
+const customStyles = {
+  content: {
+    width: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    position: "relative",
+  },
+};
+
+Modal.setAppElement("#root");
+
+const Ticket = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState("");
+
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  );
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  );
+
+  const { ticketId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //close ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket closed");
+    navigate("/tickets");
+  };
+
+  // Create note submit
+  const onNoteSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createNote({ noteText, ticketId }));
+    closeModal();
+    // testing
+    setNoteText("");
+  };
+
+  // Open/close modal
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
+    // eslint-disable-next-line
+  }, [isError, message, ticketId]);
+
+  if (isLoading || notesIsLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h3>Oops. Something went wrong.</h3>;
+  }
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url={"/tickets"} />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-us")}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+
+      {ticket.status !== "closed" && (
+        <button onClick={openModal} className="btn">
+          <FaPlus />
+          Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Note"
+      >
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className="form-group">
+            <textarea
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="Note Text"
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== "closed" && (
+        <button onClick={onTicketClose} className="btn btn-block btn-danger">
+          Close ticket
+        </button>
+      )}
+    </div>
+  );
+};
+export default Ticket;
