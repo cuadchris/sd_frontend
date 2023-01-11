debacae74eea9a67c23bffb2c8dc8b1b5f0ec4da
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-11 14:35:28 -0500

    removed scale

--- src/components/TicketItem.jsx
+++ src/components/TicketItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const TicketItem = ({ ticket }) => {
   return (
     <Link to={`/ticket/${ticket._id}`}>
-      <div className="ticket grow">
+      <div className="ticket">
         <div>{new Date(ticket.createdAt).toLocaleString("en-US")}</div>
         <div>{ticket.product}</div>
         <div className={`status status-${ticket.status}`}>{ticket.status}</div>
